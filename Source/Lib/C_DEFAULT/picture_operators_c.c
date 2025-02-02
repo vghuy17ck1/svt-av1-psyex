90cf58fbad175e50ceda41b38af8197732554a5c
Julio Barba <juliobbv@gmail.com>
2025-02-02 18:19:41 -0500

    fix: remove size check in psy-rd calculation

--- Source/Lib/C_DEFAULT/picture_operators_c.c
+++ Source/Lib/C_DEFAULT/picture_operators_c.c
@@ -86,13 +86,9 @@ uint64_t svt_spatial_psy_distortion_kernel_c(uint8_t* input, uint32_t input_offs
                                               uint8_t* recon, int32_t recon_offset, uint32_t recon_stride,
                                               uint32_t area_width, uint32_t area_height, double psy_rd) {
     uint64_t spatial_distortion = 0;
-
-    // const double psy_rd   = 2.0f;
-    const uint32_t count = area_width * area_height;
-
     uint64_t psy_distortion = 0;
 
-    if (count >= 64 && psy_rd > 0.0) {
+    if (psy_rd > 0.0) {
         uint64_t ac_distortion = svt_psy_distortion(input + input_offset, input_stride, recon + recon_offset, recon_stride, area_width, area_height);
         psy_distortion = (uint64_t)(ac_distortion * psy_rd);
     }
