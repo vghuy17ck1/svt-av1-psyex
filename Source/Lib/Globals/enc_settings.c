12680e72f396cf31ccdfc365de3377fa18bb3131
BlueSwordM <neutronpcxt@gmail.com>
2025-03-16 02:14:48 -0400

    fix: enabling psy-rd for tune 0 and better docs

--- Source/Lib/Globals/enc_settings.c
+++ Source/Lib/Globals/enc_settings.c
@@ -912,8 +912,8 @@ EbErrorType svt_av1_verify_settings(SequenceControlSet *scs) {
         return_error = EB_ErrorBadParameter;
     }
 
-    if (config->psy_rd != 0.0 && config->tune < 2) {
-        SVT_ERROR("Instance %u: PSY-RD is only supported by Tune 2 and above\n", channel_number + 1);
+    if (config->psy_rd != 0.0 && config->tune == 1) {
+        SVT_ERROR("Instance %u: PSY-RD is only supported by psycho-visually oriented tunes\n", channel_number + 1);
         return_error = EB_ErrorBadParameter;
     }
 
@@ -1216,12 +1216,12 @@ void svt_av1_print_lib_params(SequenceControlSet *scs) {
                 config->noise_norm_strength);
         }  
 
-                if (config->kf_tf_strength > 0 && config->enable_tf == 1) {
-                    SVT_INFO("SVT [config]: Keyframe TF Strength \t\t\t\t\t\t: %d\n",
-                            config->kf_tf_strength);
-                }
+        if (config->kf_tf_strength > 0 && config->enable_tf == 1) {
+            SVT_INFO("SVT [config]: Keyframe TF Strength \t\t\t\t\t\t: %d\n",
+                config->kf_tf_strength);
+        }
         
-        if (config->psy_rd > 0.0 && config->tune > 1) {
+        if (config->psy_rd > 0.0 && config->tune != 1) {
             SVT_INFO("SVT [config]: PSY-RD Strength \t\t\t\t\t\t: %.2f\n",
                     config->psy_rd);
         }
