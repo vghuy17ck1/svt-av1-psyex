e19b5b60f54b7c1b8b3fdc61fb42b8d45c22c271
BlueSwordM <neutronpcxt@gmail.com>
2025-04-12 14:07:16 -0400

    fix: tune 3 docs

--- Source/App/app_config.c
+++ Source/App/app_config.c
@@ -1266,7 +1266,7 @@ ConfigEntry config_entry_variance_boost[] = {
     {SINGLE_INPUT, MIN_CHROMA_QM_LEVEL_TOKEN, "[PSY] Min chroma quant matrix flatness, default is 8 [0-15]", set_cfg_generic_token},
     {SINGLE_INPUT, MAX_CHROMA_QM_LEVEL_TOKEN, "[PSY] Max chroma quant matrix flatness, default is 15 [0-15]", set_cfg_generic_token},
     //Noise normalisation strength
-    {SINGLE_INPUT, NOISE_NORM_STRENGTH_TOKEN, "[PSY] Noise normalization strength, default is 0, default for tune=3 is 3 [0-4]", set_cfg_generic_token},
+    {SINGLE_INPUT, NOISE_NORM_STRENGTH_TOKEN, "[PSY] Noise normalization strength, default is 0, default for tune=3 is 1 [0-4]", set_cfg_generic_token},
     //Alt-ref temporal filtering strength on keyframes
     {SINGLE_INPUT, KF_TF_STRENGTH_FILTER_TOKEN, "[PSY] Adjust alt-ref TF strength on keyframes, default is 1 (4x weaker than mainline) [0-4]", set_cfg_generic_token},
     //Psy-rd
